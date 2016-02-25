1f2f65846fd3b60301c5d9c209053dd73c1888b4
unknown <nbajaj@NBAJAJ3-WIN7L.btisystems.com>
2016-02-25 10:36:24 -0500

    my initial test (1) for selenium

---
+++ src/com/test/MyClass.java
@@ -0,0 +1,36 @@
+package com.test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class MyClass {
+	public static void main(String[] args) {
+        // declaration and instantiation of objects/variables
+        WebDriver driver = new FirefoxDriver();
+        String baseUrl = "http://newtours.demoaut.com";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+ 
+        // launch Firefox and direct it to the Base URL
+        driver.get(baseUrl);
+ 
+        // get the actual value of the title
+        actualTitle = driver.getTitle();
+ 
+        /*
+         * compare the actual title of the page witht the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        if (actualTitle.equals(expectedTitle))
+        		{
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+        
+        //close Firefox
+        driver.close();
+        
+        // exit the program explicitly
+        System.exit(0);
+    }
+ 
+}
